aa615a4d6aed97bb251e2e931dafea78535c3bfe
Mark Kuang <yk2516@log-1.hpc.nyu.edu>
2022-01-12 15:52:21 -0500

    update

---
+++ benchmark/run_source_finetune.py
@@ -0,0 +1,65 @@
+'''
+Author: Yilun Kuang
+Date: Jan 12, 2021
+status: IN-PROGRESS
+
+This script is used to finetune the bert-base-uncased model on the source domain datasets (i.i.d) 
+using the classification objective. 
+'''
+import math
+import argparse
+import numpy as np
+from datasets import load_metric
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers import TrainingArguments
+from transformers import Trainer, set_seed
+
+def main(args):
+    column_dict = {'imdb':'text','sst2':'sentence','yelp_polarity':'text'}
+    glue_lst = ["ax", "cola", "mnli", "mnli_matched", "mnli_mismatched", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+
+    def tokenize_function(examples):
+        return tokenizer(examples[column_dict[args.dataset_name]], padding="max_length", truncation=True)
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+        
+    set_seed(args.random_seed)
+
+    model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2, cache_dir=args.cache_dir)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", cache_dir=args.cache_dir)
+
+    if args.dataset_name in glue_lst:
+        raw_datasets = load_dataset("glue", args.dataset_name, cache_dir=args.cache_dir)
+    else: 
+        raw_datasets = load_dataset(args.dataset_name, cache_dir=args.cache_dir)
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    if "validation" not in raw_datasets.keys():
+        full_train_dataset = tokenized_datasets["train"]
+        full_eval_dataset = tokenized_datasets["test"]
+    else:
+        full_train_dataset = tokenized_datasets["train"]
+        full_eval_dataset = tokenized_datasets["validation"]
+
+    training_args = TrainingArguments(output_dir=args.output_dir+"/bert_"+args.dataset_name+"_finetune", overwrite_output_dir=True)
+    trainer = Trainer(
+        model=model, args=training_args, train_dataset=full_train_dataset, eval_dataset=full_eval_dataset
+    )
+    train_result = trainer.train()
+    trainer.save_model()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str)
+    parser.add_argument("--random_seed", type=int)
+    parser.add_argument("--output_dir",type=str)
+    parser.add_argument("--cache_dir", type=str, default='/scratch/yk2516/cache')
+
+    args = parser.parse_args()
+
+    main(args)
--- benchmark/run_target_evaluate.py
+++ benchmark/run_target_evaluate.py
@@ -1,10 +1,10 @@
 '''
 Author: Yilun Kuang
-Date: Jan 2, 2021
-status: COMPLETE
+Date: Jan 12, 2021
+status: IN-PROGRESS
 
-This script is used to evaluated the bert-base-uncased model that is already finetuned on the imdb datasets (i.i.d)
-using the classification objective on the sst2 dataset from the GLUE benchmark. 
+This script is used to evaluated the bert-base-uncased model that is already finetuned on the in-domain source datasets (i.i.d)
+using the classification objective on the out-of-domain target dataset. 
 '''
 import math
 import torch
@@ -19,8 +19,11 @@ from transformers import TrainingArguments
 from transformers import Trainer, set_seed
 
 def main(args):
+    column_dict = {'imdb':'text','sst2':'sentence','yelp_polarity':'text'}
+    glue_lst = ["ax", "cola", "mnli", "mnli_matched", "mnli_mismatched", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+
     def tokenize_function(examples):
-        return tokenizer(examples["sentence"], padding="max_length", truncation=True)
+        return tokenizer(examples[column_dict[args.dataset_name]], padding="max_length", truncation=True)
     def compute_metrics(eval_pred):
         softmax_func = nn.Softmax(dim=1)
         logits, labels = eval_pred
@@ -48,17 +51,26 @@ def main(args):
         tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", cache_dir=args.cache_dir)
         print('*** The tokenizer in use is bert-base-uncased ***')
 
-    raw_datasets = load_dataset("glue", args.dataset_name, cache_dir=args.cache_dir)
-    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-    full_train_dataset = tokenized_datasets["train"]
-    full_eval_dataset = tokenized_datasets["validation"]
+    if args.dataset_name in glue_lst:
+        raw_datasets = load_dataset("glue", args.dataset_name, cache_dir=args.cache_dir)
+    else: 
+        raw_datasets = load_dataset(args.dataset_name, cache_dir=args.cache_dir)
 
-    f_logits = open(args.logits_dir+"/logits"+str(args.random_seed)+".txt", "a")
-    f_logits_prob = open(args.logits_dir+"/logits_prob"+str(args.random_seed)+".txt", "a")
-    f_labels = open(args.logits_dir+"/gold_label"+str(args.random_seed)+".txt","a")
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    if "validation" not in raw_datasets.keys():
+        full_train_dataset = tokenized_datasets["train"]
+        full_eval_dataset = tokenized_datasets["test"]
+    else:
+        full_train_dataset = tokenized_datasets["train"]
+        full_eval_dataset = tokenized_datasets["validation"]
+
+    f_logits = open(args.logits_dir+"/logits"+args.model_seed+"_"+str(args.random_seed)+".txt", "a")
+    f_logits_prob = open(args.logits_dir+"/logits_prob"+args.model_seed+"_"+str(args.random_seed)+".txt", "a")
+    f_labels = open(args.logits_dir+"/gold_label"+args.model_seed+"_"+str(args.random_seed)+".txt","a")
 
     # Evaluation
-    training_args = TrainingArguments(args.output_dir+"/sst2_test")
+    training_args = TrainingArguments(output_dir=args.output_dir+"/"+args.dataset_name+"_test", overwrite_output_dir=True)
     metric = load_metric("accuracy")
     trainer = Trainer(
         model=model,
@@ -83,11 +95,11 @@ def main(args):
     trainer.log_metrics("eval", metrics)
     trainer.save_metrics("eval", metrics)
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_and_tokenizer_path", type=str, 
                             default='/scratch/yk2516/UDA_Text_Generation/benchmark/source_finetune_output/17/bert_imdb_finetune')
+    parser.add_argument("--model_seed",type=str)
     parser.add_argument("--dataset_name", type=str, default='sst2')
     parser.add_argument("--random_seed", type=int, default=17)
     parser.add_argument("--logits_dir",type=str,default='/scratch/yk2516/UDA_Text_Generation/benchmark/target_zeroshot_output')
